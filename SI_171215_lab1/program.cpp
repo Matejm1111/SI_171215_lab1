6438980a5183123b9b0384ab7f0470b6a64f9058
unknown <Matej.Mijoski@students.finki.ukim.mk>
2019-04-04 09:42:55 +0200

    Initial commit

---
+++ SI_171215_lab1/program.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void printMyName () {
+	//TODO
+}
+
+void printMyIndex () {
+	//TODO
+cout<<171215<<endl;
+}
+
+int main () {
+	//TODO
+}
